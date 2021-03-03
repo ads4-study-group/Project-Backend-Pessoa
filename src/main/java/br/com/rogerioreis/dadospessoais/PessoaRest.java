849ea38d4308dbf6083b9727e8b4f86206d4d961
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr@hotmail.com>
2021-03-03 00:15:55 -0300

    Implementado documentação Swagger

--- src/main/java/br/com/rogerioreis/dadospessoais/PessoaRest.java
+++ src/main/java/br/com/rogerioreis/dadospessoais/PessoaRest.java
@@ -13,25 +13,31 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.rogerioreis.dadospessoais.model.Pessoa;
 import br.com.rogerioreis.dadospessoais.model.PessoaDao;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController // faz a classe se comportar como webservice
-@RequestMapping("/pessoa") // esta notação configura a url do webservice
+@RequestMapping("/dadospessoais") // esta notação configura a url do webservice
+@Api(value = "API Rest Pessoa")
 public class PessoaRest {
 	
 	@Autowired // aqui é injetado uma instância da classe PessoaDao
 	private PessoaDao pessoaDao;
 	
-	@GetMapping // expõe o método get
+	@GetMapping("/pessoas") // expõe o método get
+	@ApiOperation(value = "Retorna uma lista de pessoas")
 	public List<Pessoa> lista (){	
 		return pessoaDao.findAll();
 	}
 	
 	@PostMapping // expõe o método post
+	@ApiOperation(value = "Salva e atualiza uma pessoa")
 	public void post (@RequestBody Pessoa pessoa) {
 		pessoaDao.save(pessoa);
 	}
 	
 	@DeleteMapping ("/{id}")
+	@ApiOperation(value = "Deleta uma pessoa")
 	public void delete (@PathVariable ("id") Long id) {
 		pessoaDao.deleteById(id);
 	}
